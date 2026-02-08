907c3648bd49fd25df354fe42fa3c3354cebc38a
Hudihi <Abdillahissa866@gmail.com>
2026-02-08 16:57:10 +0300

    feat: add the logger annonation Slf4j annotation

--- spring-7-rest-mvc/src/main/java/guru/springframework/spring7restmvc/controller/BeerController.java
+++ spring-7-rest-mvc/src/main/java/guru/springframework/spring7restmvc/controller/BeerController.java
@@ -1,4 +1,25 @@
 package guru.springframework.spring7restmvc.controller;
 
+
+import guru.springframework.spring7restmvc.models.Beer;
+import guru.springframework.spring7restmvc.services.BeerService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+import java.util.UUID;
+
+
+
+@Slf4j
+@AllArgsConstructor
+@Controller
 public class BeerController {
+    private final BeerService beerService;
+
+    public Beer getBeerById(UUID uuid){
+        log.debug("Get Beer by I - In controller");
+
+        return beerService.getBeers(uuid);
+    }
 }
--- spring-7-rest-mvc/src/main/java/guru/springframework/spring7restmvc/services/BeerServiceImpl.java
+++ spring-7-rest-mvc/src/main/java/guru/springframework/spring7restmvc/services/BeerServiceImpl.java
@@ -2,16 +2,24 @@ package guru.springframework.spring7restmvc.services;
 
 import guru.springframework.spring7restmvc.models.Beer;
 import guru.springframework.spring7restmvc.models.BeerStyle;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+
+@Slf4j
+@Service
 public class BeerServiceImpl implements BeerService{
 
 
     @Override
     public Beer getBeers(UUID uuid) {
+
+        log.debug("Get Beer by Id - In service. Id: {}", uuid.toString());
+
         return Beer.builder()
                 .id(uuid)
                 .version(1)
