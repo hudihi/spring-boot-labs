c2daa0d6fe5e3c95cbf353aed6f1898e082d26b2
Hudihi <Abdillahissa866@gmail.com>
2026-02-09 17:32:58 +0300

    feat: add request mapping in the controller methods for retrieve all the beers

--- spring-7-rest-mvc/src/main/java/guru/springframework/spring7restmvc/controller/BeerController.java
+++ spring-7-rest-mvc/src/main/java/guru/springframework/spring7restmvc/controller/BeerController.java
@@ -6,6 +6,10 @@ import guru.springframework.spring7restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.UUID;
@@ -15,14 +19,17 @@ import java.util.UUID;
 @Slf4j
 @AllArgsConstructor
 @Controller
+@RestController
 public class BeerController {
     private final BeerService beerService;
 
+    @RequestMapping("/api/v1/beer")
     public List<Beer> listBeers(){
         return beerService.listBeers();
     }
 
-    public Beer getBeerById(UUID uuid){
+    @RequestMapping(value = "/api/v1/beer/{beerId}",method = RequestMethod.GET)
+    public Beer getBeerById(@PathVariable("beerId") UUID uuid){
         log.debug("Get Beer by I - In controller");
 
         return beerService.getBeers(uuid);
