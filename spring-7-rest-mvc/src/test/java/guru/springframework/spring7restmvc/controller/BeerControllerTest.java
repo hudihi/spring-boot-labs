2b81714acaaeba2295e92ade89d48a46a03edff2
Hudihi <Abdillahissa866@gmail.com>
2026-02-12 18:34:10 +0300

    feat: add listbeer test function for testing the list of beers

--- spring-7-rest-mvc/src/test/java/guru/springframework/spring7restmvc/controller/BeerControllerTest.java
+++ spring-7-rest-mvc/src/test/java/guru/springframework/spring7restmvc/controller/BeerControllerTest.java
@@ -33,6 +33,16 @@ class BeerControllerTest {
     BeerServiceImpl beerServiceImpl = new BeerServiceImpl();
 
     @Test
+    void testListBeers() throws Exception {
+
+        mockMvc.perform(get("/api/v1/beer/")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.length()", is(0)));
+    }
+
+    @Test
     void getBeerById() throws Exception {
         Beer testBeer = beerServiceImpl.listBeers().get(0);
 
